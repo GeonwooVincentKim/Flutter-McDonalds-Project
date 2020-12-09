44840c65a842b8b6c02ce529e5b1ff2729a451a5
GeonwooVincentKim <kdsnop@gmail.com>
2020-12-09 10:33:36 +0900

    Delete codes that I cannot use it

--- lib/provider/provider_theme.dart
+++ lib/provider/provider_theme.dart
@@ -1,18 +1,19 @@
-import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:myTestApp_Test/shared/style/style.dart';
-   
-class DynamicThemeProvider with ChangeNotifier {  
-  bool isDarkMode = false;  
-  getDarkMode() => this.isDarkMode;  
-  void changeDarkMode(isDarkMode) {  
-    this.isDarkMode = isDarkMode;  
-    notifyListeners(); // Notify all it's listeners about update. If you comment this line then you will see that new added items will not be reflected in the list.  
-  } 
-}  
+import 'package:provider/provider.dart';
+
+
+class ProviderThemeDynamic with ChangeNotifier{
+  bool isDark = false;
+  getDarkMode() => isDark;
+  void changeMode(isDark){
+    isDark = isDark;
+    notifyListeners();
+  }
+}
 
 final darkTheme = ThemeData(
-  // primarySwatch: Colors.orange[200],
+  primarySwatch: Colors.orange[200],
   primaryColor: Colors.black,
   brightness: Brightness.dark,
   // backgroundColor: const Color(0xFF212121),
