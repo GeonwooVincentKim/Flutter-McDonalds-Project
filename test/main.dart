b3174e4adcf0c8b98d5c5e545e8b3cbecd5cc64b
GeonwooVincentKim <kdsnop@gmail.com>
2020-12-09 10:06:41 +0900

    Delete unnecessary codes

--- test/main.dart
+++ test/main.dart
@@ -9,13 +9,8 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'test_body.dart';
 import 'test_provider.dart';
 
-// void main() => SharedPreferences.getInstance().then((prefs){
-//   var darkModeOn = prefs.getBool('darkMode') ?? true;
-//   runApp(MyApp());
-// });
 void main() => runApp(MyApp());
 
-//Using Bloc
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -24,43 +19,6 @@ class MyApp extends StatelessWidget {
         ChangeNotifierProvider(create: (_) => DynamicThemeProvider()),
       ],
       child: MyAppSub()
-    // return MultiProvider(
-    //   providers: [
-
-    //   ],
-    //   child: DynamicTheme(
-    //     defaultBrightness: Brightness.dark,
-    //     data: (brightness) => ThemeData(
-    //       primarySwatch: Colors.blue,
-    //       brightness: brightness
-    //     ),
-    //     themedWidgetBuilder: (context, snapshot) {
-    //       return MaterialApp(
-    //         theme: snapshot,
-    //         initialRoute: "/",
-    //         routes: {
-    //           "/": (context) => Body(title: "HI")
-    //         },
-    //         onGenerateRoute: (settings) {
-    //           final List<String> routeGenerator = settings.name.split("/");
-    //           if(routeGenerator[0] != '') return null;
-    //           if(routeGenerator[1] == 'test'){
-    //             String subRouteID = routeGenerator[2];
-    //             return MaterialPageRoute(builder: (BuildContext context) => Test(testID: subRouteID));
-    //           }
-    //         },
-    //         onUnknownRoute: (settings) => MaterialPageRoute(builder: (BuildContext context) => Body()),
-    //       );
-    //     },
-    //   )
-    // );
-    // return StreamBuilder(
-    //   stream: bloc.darkThemeEnabled,
-    //   initialData: false,
-    //   builder: (context, snapshot) => MaterialApp(
-    //       theme: snapshot.data ? ThemeData.dark() : ThemeData.light(),
-    //       home: HomePage(snapshot.data)),
-    // );
     );
   }
 }
@@ -68,32 +26,8 @@ class MyApp extends StatelessWidget {
 class MyAppSub extends StatelessWidget {
   Widget build(BuildContext context) {
     final themeProvider = Provider.of<DynamicThemeProvider>(context); 
-    // return DynamicTheme(
-    //   defaultBrightness: Brightness.light,
-    //   data: (brightness) => ThemeData(
-    //     primarySwatch: Colors.indigo,
-    //     brightness: brightness
-    //   ),
-    //   themedWidgetBuilder: (context, theme){
-    //     return MaterialApp(
-    //       theme: theme,
-    //     );
-    //   },
-    // );
     return MaterialApp(
-      // theme: themeProvider.getTheme(), 
-      // theme: themeProvider.getDarkMode() ? ThemeData.dark() : ThemeData.light(),
       theme: themeProvider.getDarkMode() ? darkTheme : lightTheme,
-      // theme: themeProvider.getDarkMode() ? ThemeData(
-      //   // primarySwatch: Colors.black,
-      //   primaryColor: Colors.black,
-      //   brightness: Brightness.dark
-      // ) : 
-      // ThemeData(
-      //   // primarySwatch: Colors.white,
-      //   primaryColor: BasicAppBarColor,
-      //   brightness: Brightness.light
-      // ),
       initialRoute: "/",
       routes: {
         "/": (context) => TestBody(),
