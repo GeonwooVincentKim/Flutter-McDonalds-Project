5e3f2022192921c637b2f58b6ae84be17dfe3e9c
GeonwooVincentKim <kdsnop@gmail.com>
2021-01-04 10:24:12 +0900

    Not to use type

--- lib/screens/main_screen/menu.dart
+++ lib/screens/main_screen/menu.dart
@@ -47,7 +47,8 @@ class _MenuState extends State<Menu> {
           // final Map<String, dynamic> menuFilter = Provider.of<FilterProvider>(context).orderFilters;
           // Get all items of list that match to CategoryList's type.
           List<MenuModel> menuList = [];
-          menuList = menu.menuList.where((menu) => menu.type == cate.type).toList();
+          menuList = menu.menuList.where((menu) => menu.id.contains(cate.categoryID)).toList();
+          // menuList = menu.menuList.where((menu) => menu.type == cate.type).toList();
 
           return menuList.length == 0 || menuList.length == null ?
             Center(child: Text("NOO!!!")) :
